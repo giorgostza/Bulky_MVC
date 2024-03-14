17cf8fbf5bba4b5d56745a121ea68ad00f778d09
George Tzavellas <george.tzavellas@Peoplecert.onmicrosoft.com>
2024-03-14 19:42:47 +0200

    CRUD Product, ForeignKey Category,ViewBag-ViewData

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -15,12 +15,16 @@ namespace Bulky.DataAccess.Repository
 
         public ICategoryRepository Category { get; private set; }
 
+        public IProductRepository Product { get; private set; }
+
         public UnitOfWork(ApplicationDbContext db) 
         {
         
             _db = db;
 
             Category = new CategoryRepository(_db);
+
+            Product = new ProductRepository(_db);
         
         }
 
