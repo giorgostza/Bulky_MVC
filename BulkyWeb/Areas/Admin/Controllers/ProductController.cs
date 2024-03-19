704c266784f2de3f786eb92419ff6e25f2018b18
George Tzavellas <george.tzavellas@Peoplecert.onmicrosoft.com>
2024-03-19 17:22:51 +0200

    ViewModel, Upload-Edit Images

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Models.VieModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -13,13 +14,15 @@ namespace BulkyWeb.Areas.Admin.Controllers
     {
 
         private readonly IUnitOfWork _unitOfWork;
+
+        private readonly IWebHostEnvironment _webHostEnvironment;
         
 
-        public ProductController(IUnitOfWork unitOfWork)
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
 
             _unitOfWork = unitOfWork;
-
+            _webHostEnvironment = webHostEnvironment;
         }
 
        
@@ -35,33 +38,69 @@ namespace BulkyWeb.Areas.Admin.Controllers
 
         
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)    // Update-Insert= Upsert
         {
 
 
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
-               .GetAll().Select(u => new SelectListItem
-               {
+            ProductVM productVM = new()
+            {
+
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
                    Text = u.Name,
                    Value = u.Id.ToString()
 
-               });
+               }),
+
+                Product =new Product()
+
+
+            };
+
+
+            if (id==null || id==0)
+            {
+                //Create
+
+                return View(productVM);
+            }
+            else 
+            {
+                // Update
+
+                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+                return View(productVM);
+            }
+            
+
+
+
+            //IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
+            //   .GetAll().Select(u => new SelectListItem
+            //   {
+            //       Text = u.Name,
+            //       Value = u.Id.ToString()
+
+            //   });
 
             // ViewBag.CategoryList = CategoryList;          (1)  ViewBag
 
-            ViewData["CategoryList"] = CategoryList;       //(2)  ViewData  
+            // ViewData["CategoryList"] = CategoryList;       (2)  ViewData  
+
+            //return View();
 
 
-            return View();
+
         }
 
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
         {
             #region ASP Validation Summary
 
-            if (obj.Title == obj.Author)
+            if (productVM.Product.Title == productVM.Product.Author)
             {
+
 
                 ModelState.AddModelError("Author", "The Author cannot exactly match the Title.");
 
@@ -72,65 +111,82 @@ namespace BulkyWeb.Areas.Admin.Controllers
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath= Path.Combine(wwwRootPath, @"images\product");
+
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        // Delete the old image
+
+                        var oldImagePath = Path.Combine(wwwRootPath,productVM.Product.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+
+
+                    }
+
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath,fileName),FileMode.Create))
+                    {
+
+                        file.CopyTo(fileStream);
+
+                    }
+
+
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+
+
+                }
+
+
+                if (productVM.Product.Id==0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
+
+
+                
                 _unitOfWork.Save();
 
                 TempData["success"] = "Product created successfully";
 
                 return RedirectToAction("Index", "Product");
             }
+            else
+            {
+
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
 
 
-            return View();
+                });
+
+
+
+                return View(productVM);
+            }
+
+
+          
 
         }
 
        
-
-
-        public IActionResult Edit(int? id)
-        {
-
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-           
-
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-
-
-            return View(productFromDb);
-
-        }
-
-
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-
-
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-
-                TempData["success"] = "Product updated successfully";
-
-                return RedirectToAction("Index", "Product");
-            }
-
-
-            return View();
-
-        }
-
-      
 
 
         public IActionResult Delete(int? id)
