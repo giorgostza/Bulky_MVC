29dd7c7893187db18daa60f86ea6bf5d5ca3a9cf
ΓΙΩΡΓΟΣ ΤΖΑΒΕΛΛΑΣ <giorgostza91@gmail.com>
2025-11-18 14:19:42 +0200

    Summary POST

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -18,8 +18,23 @@ namespace Bulky.Utility
 
         public const string Role_Employee = "Employee";
 
-        
 
+
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved= "Approved";
+        public const string StatusInProcess= "Processing";
+        public const string StatusShipped= "Shipped";
+        public const string StatusCancelled= "Cancelled";
+        public const string StatusRefunded= "Refunded";
+
+
+
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "Approved For Delayed Payment";
+        public const string PaymentStatusRejected = "Rejected";
 
 
 
